43c02dc542648a29f44c3b4128d1abe720f285c7
Gabriel <gabrielpires2991@gmail.com>
2022-12-16 12:34:08 -0300

    Background colors separated by pokemons types

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,17 @@
 
-
-
-const offset = 0
-const limit = 10
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
-
 function convertPokemonToLi(pokemon) {
     return `
-        <li class="pokemon">
-                    <span class="number">#001</span>
+        <li class="pokemon ${pokemon.type}">
+                    <span class="number">#${pokemon.number}</span>
                     <span class="name">${pokemon.name}</span>
 
                     <div class="detail">
                         <ol class="types">
-                            <li class="type">grass</li>
-                            <li class="type">poison</li>
+                            ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
                         </ol>   
 
-                        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
+                        <img src="${pokemon.photo}"
+                             alt="${pokemon.name}">
                     </div>
                 </li>
             `
@@ -28,15 +22,19 @@ const pokemonList = document.getElementById('pokemonList');
 // requisita a url, retorna a promessa quando o fetch der certo realiza o console.log
 // interface da promisse é baseada no bloco try-catch-finally
 
-fetch(url)
-    .then((response) => response.json())
-    .then((jsonBody) => jsonBody.results)
-    .then((pokemons) => {
+    pokeApi.getPokemons().then((pokemons = []) => {
 
-        for (let i = 0; i < pokemons.length; i++) {
-            const pokemon = pokemons[i];
-            pokemonList.innerHTML += convertPokemonToLi(pokemon);
-        }
+        pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('')
+
+
+        // const listItems = [];
+
+        // for (let i = 0; i < pokemons.length; i++) {
+        //     const pokemon = pokemons[i];
+        //     listItems.push(convertPokemonToLi(pokemon)); 
+        // }
+
+        console.log(pokemonList);
+
     })
-    .catch ((error) => console.error(error))
 
